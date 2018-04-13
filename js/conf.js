f1a12dd86bfeb21045eda527711e29198755e769
Nayan Khedkar <nayan.khedkar@exultcorp.com>
2018-04-13 17:30:01 +0530

    typo

--- js/conf.js
+++ js/conf.js
@@ -1,6 +1,6 @@
 define(function(require) {
 
-    // Used to determine cofigarable properties
+    // Used to determine configurable properties
     var CONF = {
     	'MAX_WIDTH':340,     //when windows width is less than tooltip outer width * PADDINGFACTOR
     	'ANIMATE_TOP':5,     //animate tooltip-box when appear
